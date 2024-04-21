f1ba7d53cc299ec9dbf0a874c26761148090450d
cyrilsl <lucascyrilsamuel@gmail.com>
2024-04-21 23:36:05 +0530

    /home changed

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,16 +7,12 @@ interface Product {
   // Add other properties as needed
 }
 
-export default function Storefront({
-  params,
-}: {
-  params: { subdomain: string };
-})  {
+export default function Storefront()  {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-        Domain URL : {params.subdomain} : Main Store
+        Domain URL : Main Page
         </p>
       </div>
     </main>
