abfc1708e2ada8141299e8fc84cb63a0a336f6db
cyrilsl <lucascyrilsamuel@gmail.com>
2024-05-06 21:32:41 +0530

    hoe dashbard mild edit

--- src/app/home/dashboard/page.tsx
+++ src/app/home/dashboard/page.tsx
@@ -1,28 +1,7 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import { useAdminGetSession, useAdminCustomQuery, useAdminCustomDelete } from "medusa-react";
-import { Checkbox, Label,  Heading  } from "@medusajs/ui";
-import { useAdminStore, useAdminUser } from "medusa-react"
-import Link from "next/link"
-import {
-  Activity,
-  ArrowUpRight,
-  CircleUser,
-  CreditCard,
-  DollarSign,
-  Menu,
-  Package2,
-  Search,
-  Users,
-} from "lucide-react"
+import { useAdminStore } from "medusa-react"
 
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
-import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
@@ -31,24 +10,6 @@ import {
   CardTitle,
   CardFooter
 } from "@/components/ui/card"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Input } from "@/components/ui/input"
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
 
 type Props = {
   userId: string
