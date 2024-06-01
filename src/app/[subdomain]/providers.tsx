b30d3ef3b84e1aa63f2cc2dbf1710763270f1e57
cyrilsl <lucascyrilsamuel@gmail.com>
2024-06-02 04:18:00 +0530

    posthog provider added

--- src/app/[subdomain]/providers.tsx
+++ src/app/[subdomain]/providers.tsx
@@ -4,11 +4,17 @@ import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 
 if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-  })
+  const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+  const posthogHost = process.env.NEXT_PUBLIC_POSTHOG_HOST;
+
+  if (posthogKey) {
+    posthog.init(posthogKey, {
+      api_host: posthogHost,
+      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    });
+  }
 }
+
 export function CSPostHogProvider({ children }) {
-    return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
 }
